67273c3e68fd683b9517a5f6ee23eb41d90e0885
LeoAraujo30 <leonardo.araujo300301@gmail.com>
2023-03-14 18:15:32 -0300

    requisitos 7 8 e 9 feitos

--- tech_news/analyzer/search_engine.py
+++ tech_news/analyzer/search_engine.py
@@ -1,13 +1,44 @@
+from tech_news.database import db
+import datetime
+
+
 # Requisito 7
 def search_by_title(title):
-    """Seu código deve vir aqui"""
+    list = db.news.find(
+        {"title": {"$regex": title, "$options": "i"}},
+        {"_id": False, "title": True, "url": True}
+    )
+    result = [tuple((dict["title"], dict["url"])) for dict in list]
+    return result
 
 
 # Requisito 8
+def validate_date(date):
+    try:
+        datetime.date.fromisoformat(date)
+        return True
+    except ValueError:
+        return False
+
+
 def search_by_date(date):
-    """Seu código deve vir aqui"""
+    if validate_date(date):
+        list_date = date.split("-")
+        list = db.news.find(
+            {"timestamp": f"{list_date[2]}/{list_date[1]}/{list_date[0]}"},
+            {"_id": False, "title": True, "url": True}
+        )
+        result = [tuple((dict["title"], dict["url"])) for dict in list]
+        return result
+    else:
+        raise ValueError("Data inválida")
 
 
 # Requisito 9
 def search_by_category(category):
-    """Seu código deve vir aqui"""
+    list = db.news.find(
+        {"category": {"$regex": category, "$options": "i"}},
+        {"_id": False, "title": True, "url": True}
+    )
+    result = [tuple((dict["title"], dict["url"])) for dict in list]
+    return result
